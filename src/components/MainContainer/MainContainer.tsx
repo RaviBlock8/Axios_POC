6079a9671010b4e4be4bd9ea6383e9e09482b211
RaviBlock8 <ravi83165@gmail.com>
2020-08-26 11:10:39 +0530

    add qtable

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -3,13 +3,16 @@ import SellOrdersContainer from "../SellOrders/SellOrders.container";
 import BuyOrdersContainer from "../BuyOrders/BuyOrders.container";
 import { Box } from "@material-ui/core";
 import { styled } from "@material-ui/styles";
+import QTable from "../QTable/QTable";
 
 export default function MainContainer() {
   return (
-    <Main>
-      <SellOrdersContainer />
-      <BuyOrdersContainer />
-    </Main>
+    <>
+      <Main>
+        <SellOrdersContainer />
+        <BuyOrdersContainer />
+      </Main>
+    </>
   );
 }
 
